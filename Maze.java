e0c638fb48ea06e38a1e2fb55117fd7c07762f0d
Cole Saramaki <sara7360@ad.wlu.ca>
2016-11-13 15:24:38 -0500

    Started Main Program

--- Maze.java
+++ Maze.java
@@ -5,53 +5,11 @@ public class Maze{
 	ArrayList<ArrayList<Node>> board;
 	int height;
 	int width;
-	public class Node{
-		int g;
-		int h;
-		int row;
-		int col;
-		boolean isObject;
-		public Node(int g, int h, int row; int col; boolean isObject){
-			this.g=g;
-			this.h=h;
-			this.row=row;
-			this.col=col;
-			this.isObject=isObject;
-
-		}
 	}
+	
 	public Maze(int height, int width ){
 		this.board=new ArrayList<ArrayList<Node>>();
 		this. height=height;
 		this.width=width;
 	}
-
-	public static void initMaze(String filePath){
-		String line = null;
-		BufferedReader reader=null;
-
-		try{
-			FileReader file = new FileReader(filePath);
-			reader = new BufferedReader(file);
-		}
-		catch(FileNotFoundException f){
-			System.out.println("Error: File not found");
-		}
-
-		try{
-			int rowIndex=0;
-			while((line = reader.readLine()) != null){
-				String[] mazeRow=line.split(" ");
-
-				System.out.println(mazeRow);
-
-			}
-
-		}
-		catch (IOException e){
-			System.out.println("Error: Cannot readfile");
-		}
-}
-
-
 }
