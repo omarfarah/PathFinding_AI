811893b9ed133d23b1171176f80dfee92be3dad2
perlanie <perlaniee@hotmail.com>
2016-11-13 15:26:10 -0500

    organizing code

--- Maze.java
+++ Maze.java
@@ -20,9 +20,9 @@ public class Maze{
 
 		}
 	}
-	public Maze(int height, int width ){
+	public Maze(int height, int width){
 		this.board=new ArrayList<ArrayList<Node>>();
-		this. height=height;
+		this.height=height;
 		this.width=width;
 	}
 
