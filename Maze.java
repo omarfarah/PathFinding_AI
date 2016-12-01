fef0fcbed0fe3bd33900af27a96b8e9146e29620
omarfarah <fara6980@mylaurier.ca>
2016-12-01 14:50:45 -0500

    Adding docs

--- Maze.java
+++ Maze.java
@@ -2,6 +2,15 @@ import java.io.*;
 import java.util.*;
 import java.lang.Math;
 
+/*===============================================================
+Maze Class: Create an instance for the input Maze.	
+	@param height;	The height of the Maze
+	@param width;	The width of the Maze
+	@param board;	The node instance
+	@param endX;	Last positon of the board X
+	@param endY;	Last positon of the board Y
+================================================================*/
+	
 public class Maze{
 	Node[][] board;
 	int height, width, endX, endY;
@@ -21,6 +30,9 @@ public class Maze{
 		}	
 	}
 
+	/*===============================================================
+	Copy: Create a copy of the Maze
+	================================================================*/
 	public Maze copyMaze(){
 		Node[][] newBoard = new Node[this.height][this.width];
 		for(int r=0;r<this.height;r++){
@@ -97,7 +109,10 @@ public class Maze{
 			return false;
 		}
 	}
-
+	
+	/*=================================================================
+	getDestination: returns the goal state
+	==================================================================*/
 	public int [] getDestination(){
 		int[] destintation={this.endX,this.endY};
 		return destintation;
--- Node.java
+++ Node.java
@@ -1,6 +1,20 @@
 import java.io.*;
 import java.util.*;
 
+/*===============================================================
+Node Class: 
+This class creates a instance for each node (position on the maze) which includes
+the specific nodes function values (g,h and f) its row and column position (x,y),
+if the node is a object, and each parent node it has.
+
+		@param g;			g value
+		@param h;			h value
+		@param row;			row position of node
+		@param col;			column position of node
+		@param isObject;	object check
+		@param parent;		The parents
+================================================================*/
+
 public class Node{
 	private double g=0;
 	private double h;
@@ -17,25 +31,28 @@ public class Node{
 	}
 
 	/*===============================================================
-	getRow: sets the H value of the node
+	getRow: gets the row value of the node
 	================================================================*/
 	public int getRow(){
 		return this.row;
 	}
 	
 	/*===============================================================
-	getRow: sets the H value of the node
+	getCol: gets the column value of the node
 	================================================================*/
 	public int getCol(){
 		return this.col;
 	}
-
+	
+	/*===============================================================
+	getDistance: gets the distance from current Node to Final
+	================================================================*/
 	public double getDistance(){
 		return ((double)this.g + (double)this.h);
 	}
 
 	/*===============================================================
-	getRow: sets the H value of the node
+	isBool: checks if Node is an object
 	================================================================*/
 	public boolean isBool(){
 		return this.isObject;
@@ -48,6 +65,9 @@ public class Node{
 		this.h=hValue;
 	}
 
+	/*===============================================================
+	setH: gets the H value of the node
+	================================================================*/
 	public double getH(){
 		return this.h;
 	}
@@ -59,21 +79,34 @@ public class Node{
 		this.g=gValue;
 	}
 
+	/*===============================================================
+	getG: gets the G value of the node
+	================================================================*/
 	public double getG(){
 		return this.g;
 	}
 
+	/*===============================================================
+	setParent: sets the parent ndoes for current node
+	================================================================*/
 	public void setParent(Node parent){
 		this.parent=parent;
 	}
-
+	/*===============================================================
+	getParent: gets the parent ndoes for current node
+	================================================================*/
 	public Node getParent(){
 		return this.parent;
 	}
-
+	/*===============================================================
+	setSymbol: sets the symbol
+	================================================================*/
 	public void setSymbol(String symbol){
 		this.symbol=symbol;
 	}
+	/*===============================================================
+	getSymbol: gets the symbol
+	================================================================*/
 	public String getSymbol(){
 		return this.symbol;
 	}
